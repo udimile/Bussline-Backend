1cf6a5f28d17b12840d58467ec3324a1f4f240a0
udimile <udimile@gmail.com>
2024-10-30 01:01:46 -0300

    create login

---
+++ src/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(process.env.PORT || 3000);
---
+++ src/middlewares/loginMiddleware.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+const knex = require('../connection/connection');
+
+const hashPassword = process.env.SENHA_JWT;
+
+const verifyLogin = async (req, res, next) => {
+    const {authorization} = req.headers;
+
+    if (!authorization) {
+        return res.status(401).json({message: "Não autorizado!"});
+    }
+
+    try {
+        const token = authorization.split(' ')[1];
+
+        const {id} = jwt.verify(token, hashPassword);
+
+        const userExists = await knex('users').where({id}).first()
+
+        if (!userExists) {
+            return res.status(401).json({message: "Não autorizado!"})
+        }
+
+        const {password, ...user} = userExists;
+
+        req.user = user;
+
+        next()
+    } catch (error) {
+        return res.status(500).json({message: "Erro interno do servidor."})
+    }
+}
+
+module.exports = verifyLogin;
