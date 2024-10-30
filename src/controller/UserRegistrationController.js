8d811d161893695235f510ec0be2a4933b1127f2
udimile <udimile@gmail.com>
2024-10-30 18:11:23 -0300

    create registration

---
+++ src/controller/UserRegistrationController.js
@@ -0,0 +1,57 @@
+const knex = require('../connection/connection')
+const bcrypt = require('bcrypt')
+
+const createStudent = async (req, res) => {
+    const {name, email, password, cpf, studentId} = req.body
+
+    try {
+        const user = await knex('users').where({email: email});
+
+        if (user.length > 0) {
+            return res.status(409).json({message: "Email já existe!"})
+        }
+
+        const passwordBcrypt = await bcrypt.hash(password, 10)
+
+        const newUser = await knex('user').insert({
+            name,
+            email,
+            password: passwordBcrypt,
+            studentId,
+            cpf,
+        }).returning(['name', 'email', 'password', 'student_id', 'cpf']);
+
+        return res.status(201).json(newUser[0])
+
+    } catch (error) {
+        return res.status(500).json({message: "Erro interno do servidor. Tente novamente mais tarde."})
+    }
+}
+
+const createGuardian = async (req, res) => {
+    const {name, email, password, cpf} = req.body
+
+    try {
+        const user = await knex('users').where({email: email});
+
+        if (user.length > 0) {
+            return res.status(409).json({message: "Email já existe!"})
+        }
+
+        const passwordBcrypt = await bcrypt.hash(password, 10)
+
+        const newUser = await knex('user').insert({
+            name,
+            email,
+            password: passwordBcrypt,
+            cpf,
+        }).returning(['name', 'email', 'password', 'cpf']);
+
+        return res.status(201).json(newUser[0])
+
+    } catch (error) {
+        return res.status(500).json({message: "Erro interno do servidor. Tente novamente mais tarde."})
+    }
+}
+
+module.exports = {createGuardian, createStudent}
---
+++ src/middlewares/userRegistrationMiddleware.js
@@ -0,0 +1,12 @@
+const validate = (joiSchema) => async (req, res, next) => {
+    try {
+        await joiSchema.validateAsync(req.body)
+        next()
+    } catch (error) {
+        return res.status(400).json({message: error.message})
+    }
+
+}
+
+module.exports = validate;
+
