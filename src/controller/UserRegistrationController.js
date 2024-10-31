3c6dae70e23b9264b97dee1d2db1b462f0c92d49
udimile <udimile@gmail.com>
2024-10-31 16:24:12 -0300

    registration update

--- src/controller/UserRegistrationController.js
+++ src/controller/UserRegistrationController.js
@@ -2,17 +2,22 @@ const knex = require('../connection/connection')
 const bcrypt = require('bcrypt')
 
 const createStudent = async (req, res) => {
-    const {name, email, password, cpf, studentId} = req.body
+    const {name, email, password, cpf, studentId, school} = req.body
 
     try {
+<<<<<<< Updated upstream
         const user = await knex('users').where({email: email});
+=======
+        const existingUser = await knex('users').where({email: email});
+>>>>>>> Stashed changes
 
-        if (user.length > 0) {
+        if (existingUser.length > 0) {
             return res.status(409).json({message: "Email já existe!"})
         }
 
         const passwordBcrypt = await bcrypt.hash(password, 10)
 
+<<<<<<< Updated upstream
         const newUser = await knex('user').insert({
             name,
             email,
@@ -20,8 +25,36 @@ const createStudent = async (req, res) => {
             studentId,
             cpf,
         }).returning(['name', 'email', 'password', 'student_id', 'cpf']);
+=======
+        let newStudent;
+        let newUser;
+>>>>>>> Stashed changes
 
-        return res.status(201).json(newUser[0])
+        await knex.transaction(async trx => {
+            newUser = await trx('user').insert({
+                name,
+                email,
+                password: passwordBcrypt,
+                type: 'student',
+            }).returning(['id', 'name', 'email', 'type' ]);
+
+            const userId = newUser[0].id;
+
+            newStudent = await trx('student').insert({
+                user_id: userId,
+                cpf,
+                ra: studentId,
+                school,
+            }).returning(['*']);
+
+
+        })
+
+
+        return res.status(201).json({
+            usuario: newUser[0],
+            estudante: newStudent[0]
+        })
 
     } catch (error) {
         return res.status(500).json({message: "Erro interno do servidor. Tente novamente mais tarde."})
@@ -32,22 +65,41 @@ const createGuardian = async (req, res) => {
     const {name, email, password, cpf} = req.body
 
     try {
+<<<<<<< Updated upstream
         const user = await knex('users').where({email: email});
+=======
+        const existingUser = await knex('users').where({email: email});
+>>>>>>> Stashed changes
 
-        if (user.length > 0) {
+        if (existingUser.length > 0) {
             return res.status(409).json({message: "Email já existe!"})
         }
 
         const passwordBcrypt = await bcrypt.hash(password, 10)
 
-        const newUser = await knex('user').insert({
-            name,
-            email,
-            password: passwordBcrypt,
-            cpf,
-        }).returning(['name', 'email', 'password', 'cpf']);
+        let newGuardian;
+        let newUser;
 
-        return res.status(201).json(newUser[0])
+        await knex.transaction(async trx => {
+            newUser = await trx('user').insert({
+                name,
+                email,
+                password: passwordBcrypt,
+                type: 'guardian',
+            }).returning(['id','name', 'email']);
+
+            const userId = newUser[0].id;
+
+            newGuardian = await trx('guardian').insert({
+                user_id: userId,
+                cpf,
+            }).returning(['*']);
+        })
+
+        return res.status(201).json({
+            usuario: newUser[0],
+            responsavel: newGuardian[0]
+        })
 
     } catch (error) {
         return res.status(500).json({message: "Erro interno do servidor. Tente novamente mais tarde."})
--- src/routes/userRegistrationMiddleware.js
+++ src/routes/userRegistrationMiddleware.js
@@ -2,7 +2,7 @@ const express = require('express');
 const registrationRouter = express.Router();
 
 const {createStudent, createGuardian} = require('../controller/UserRegistrationController')
-const validate = require('../middlewares/userRegistrationMiddleware');
+const validate = require('../middlewares/requestMiddleware');
 const {userRegistrationStudentSchema, userRegistrationGuardianSchema} = require('../validations/userRegistrationSchema');
 
 registrationRouter.post('/student', validate(userRegistrationStudentSchema), createStudent);
